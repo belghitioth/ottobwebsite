05eab059e9480237ab9e8e05fa24c3a7d06f98b0
othmaneeb <belghitioth@gmail.com>
2019-05-11 15:09:09 +0200

    first boot commit

---
+++ shipitfile.js
@@ -0,0 +1,17 @@
+module.exports = shipit => {
+    // Load shipit-deploy tasks
+    require('shipit-deploy')(shipit)
+
+    shipit.initConfig({
+      default: {
+        repositoryUrl: 'git@github.com:belghitioth/pokemon-app.git',
+        deployTo: '/var/apps/pokemon-app',
+        deleteOnRollback : false,
+        keepReleases: 2
+      },
+      staging: {
+        servers: 'ec2-user@ec2-35-180-140-182.eu-west-3.compute.amazonaws.com',
+        key: '~/OthKey.pem',
+      }
+    });
+  };
